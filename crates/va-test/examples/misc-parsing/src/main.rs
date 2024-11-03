277f992e2d66a046907308519e40e1dfcd4280a3
Bryan Parno <parno@cmu.edu>
2024-11-03 15:20:13 -0500

    Fix for ensures with a `,` and a semi-colon at the end

--- crates/va-test/examples/misc-parsing/src/main.rs
+++ crates/va-test/examples/misc-parsing/src/main.rs
@@ -1,30 +1,11 @@
 verus! {
 
-//fn test(s: Set<int>) {
-//    let x = s.choose();
-//    choose|x: int| f1(x)
-//}
-//
-
-fn simple() {
-    let x_witness = choose|x: int| f1(x);
-}
-/*
-fn test_choose() {
-    assume(exists|x: int| f1(x) == 10);
-    proof {
-        let x_witness = choose|x: int| f1(x) == 10;
-        assert(f1(x_witness) == 10);
-    }
-
-    assume(exists|x: int, y: int| f1(x) + f1(y) == 30);
-    proof {
-        let (x_witness, y_witness): (int, int) = choose|x: int, y: int| f1(x) + f1(y) == 30;
-        assert(f1(x_witness) + f1(y_witness) == 30);
-    }
-}
-*/
-
+fn closed_under_incl()
+    requires
+        Self::op(a, b).valid(),
+    ensures
+        a.valid(),
+;
 
 } // verus!
 
