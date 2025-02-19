2ce9ab188f4e23cb229e8d07ac848b4966699bc9
Bryan Parno <parno@cmu.edu>
2025-02-19 15:12:52 -0500

    Remove no-longer-needed script

--- build_gunzip_chmod.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/bash
-cargo xtask dist --proof-action
-gunzip dist/verus-analyzer-x86_64-unknown-linux-gnu.gz
-chmod +x dist/verus-analyzer-x86_64-unknown-linux-gnu
-
-# cargo watch -- ./build_gunzip_chmod.sh
