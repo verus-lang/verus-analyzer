2d3c9c85da4b23283e8832fc5bfa551320676e25
Bryan Parno <parno@cmu.edu>
2025-02-20 11:41:54 -0500

    Allow struct expressions to follow Verus's triple operators. Fixes #42.

--- crates/va-test/examples/misc-parsing/src/main.rs
+++ crates/va-test/examples/misc-parsing/src/main.rs
@@ -2,19 +2,10 @@ use vstd::prelude::*;
 
 verus! {
 
-pub(crate) proof fn binary_ops<A>(a: A, x: int) {
-//    assert(false ==> true);
-//    assert(true && false ==> false && false);
-//    assert(!(true && (false ==> false) && false));
-//    assert(false ==> false ==> false);
-//    assert(false ==> (false ==> false));
-//    assert(!((false ==> false) ==> false));
-//    assert(false <== false <== false);
-//    assert(!(false <== (false <== false)));
-//    assert((false <== false) <== false);
-    assert(2 + 2 !== 3);
-//    assert(a == a);
-//    assert(false <==> true && false);
+struct Foo { x: nat }
+
+spec fn foo(f: Foo) -> bool {
+    &&& f == Foo { x: 3 }
 }
 
 } // verus!
