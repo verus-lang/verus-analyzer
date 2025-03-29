6fb877119f4007709332391ea54cc22b054ab61f
Bryan Parno <parno@cmu.edu>
2025-03-29 14:42:30 -0400

    Add support for visibility qualifiers on `open`, in line with verus-lang/verus#1553

--- crates/syntax/src/lib.rs
+++ crates/syntax/src/lib.rs
@@ -654,6 +654,10 @@ fn verus_walkthrough2() {
             // function visible to crate, body visible to module
             x / 2 + y / 2
         }
+        pub open (crate) spec fn test1() {}
+        pub open (in foo) spec fn test2() {}
+        pub open (in crate::m) spec fn test3() { }
+        pub open (super) spec fn test4() {}
     }";
 
     let parse = SourceFile::parse(source_code, Edition::Edition2024);
