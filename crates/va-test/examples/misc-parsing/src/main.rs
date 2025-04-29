38292dea20210b491eac90be678e2d290a7f0feb
Bryan Parno <parno@cmu.edu>
2025-04-29 10:09:57 -0400

    Add support for curly-brace lists with `broadcast use`. See verus-lang/verus#1571.

--- crates/va-test/examples/misc-parsing/src/main.rs
+++ crates/va-test/examples/misc-parsing/src/main.rs
@@ -2,50 +2,30 @@ use vstd::prelude::*;
 
 verus! {
 
-trait T1 {
-    proof fn my_function_decl(&self, i: int, j: int) -> (r: int)
-        requires
-            0 <= i < 10,
-            0 <= j < 10,
-        ensures
-            i <= r,
-            j <= r,
-    ;
+mod m5 {
+broadcast use
+    super::raw_ptr::group_raw_ptr_axioms,
+    super::set_lib::group_set_lib_axioms,
+    super::set::group_set_axioms,
+;
+broadcast use
+    super::raw_ptr::group_raw_ptr_axioms,
+    super::set_lib::group_set_lib_axioms,
+    super::set::group_set_axioms;
+broadcast use super::raw_ptr::group_raw_ptr_axioms;
+broadcast use super::set_lib::group_set_lib_axioms;
+broadcast use super::set::group_set_axioms;
+broadcast use {
+    super::raw_ptr::group_raw_ptr_axioms,
+    super::set_lib::group_set_lib_axioms,
+    super::set::group_set_axioms};
+broadcast use {
+    super::raw_ptr::group_raw_ptr_axioms,
+    super::set_lib::group_set_lib_axioms,
+    super::set::group_set_axioms,};
+broadcast use {super::set::group_set_axioms};
+broadcast use {super::set::group_set_axioms,};
 
-    /// A trait function may have a default (provided) implementation,
-    /// and this defaults may have additional ensures specified with default_ensures
-    fn my_function_with_a_default(&self, i: u32, j: u32) -> (r: u32)
-        requires
-            0 <= i < 10,
-            0 <= j < 10,
-        ensures
-            i <= r,
-            j <= r,
-        default_ensures
-            i == r || j == r,
-        {
-            if i >= j { i } else { j }
-        }
-}
-
-trait T2 {
-    fn f(i: u32) -> (r: u32)
-        requires
-            (builtin::default_ensures)(true),
-        default_ensures
-            r <= i,
-    {
-        i / 2
-    }
-}
-
-trait T3 {
-    fn f(i: u32) -> (r: u32)
-        default_ensures
-            r <= i,
-    {
-        i / 2
-    }
 }
 
 } // verus!
