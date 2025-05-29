e4e4bcbaa3a48fcf95326b25ca34525daa5c4ea5
Bryan Parno <parno@cmu.edu>
2025-05-29 09:42:13 -0400

    Bump the expected version of Rust to mirror Verus's recent upgrade (verus-lang/verus#1385)

--- editors/code/src/bootstrap.ts
+++ editors/code/src/bootstrap.ts
@@ -222,8 +222,8 @@ const execFileAsync = promisify(execFile);
 export async function validRustToolchain(): Promise<Boolean> {
     // TODO: Add a config flag for the expected toolchain version
     const TOOLCHAIN_FULL = 1;
-    const TOOLCHAIN_MAJOR = 82;
-    const TOOLCHAIN_MINOR = 0;
+    const TOOLCHAIN_MAJOR = 85;
+    const TOOLCHAIN_MINOR = 1;
 
     const { path: rustup_executable } = await findRustup();
     if (!rustup_executable) {
