73ddff71a84d5c5ab8495b836a64365679e1707a
Daniel Schoepe <schoeped@amazon.com>
2026-04-20 09:34:54 -0700

    Bump Rust version to 1.95.0

--- editors/code/src/bootstrap.ts
+++ editors/code/src/bootstrap.ts
@@ -241,7 +241,7 @@ const execFileAsync = promisify(execFile);
 export async function validRustToolchain(): Promise<Boolean> {
     // TODO: Add a config flag for the expected toolchain version
     const TOOLCHAIN_FULL = 1;
-    const TOOLCHAIN_MAJOR = 94;
+    const TOOLCHAIN_MAJOR = 95;
     const TOOLCHAIN_MINOR = 0;
 
     const { path: rustup_executable } = await findRustup();
